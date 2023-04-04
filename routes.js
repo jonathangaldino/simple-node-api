d7201d901a2fbdf865e2bceb1ecb8834f23cd36b
Jonathan Galdino <johnzballad@gmail.com>
2023-04-04 01:39:06 -0300

    just a hello world

--- routes.js
+++ routes.js
@@ -3,6 +3,10 @@ const todos = require("./todos");
 
 const router = express.Router();
 
+router.get("/", function(req, res) {
+  res.json({ message: "Hello world!"})
+});
+
 router.get("/todos", function(req, res) {
   res.json(todos);
 });
